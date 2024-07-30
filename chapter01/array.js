7687002e9c9d47964b0258fb2b978ace611a1271
tzy <2298320493@qq.com>
2024-07-30 11:03:28 +0800

    基础

---
+++ chapter01/array.js
@@ -0,0 +1,7 @@
+let starName = ["刘德华", "张学友", "黎明", "郭富城"]
+
+console.log(starName[0])
+console.log(starName[1])
+console.log(starName[2])
+console.log(starName[3])
+
--- chapter01/bianLiang.js
+++ chapter01/bianLiang.js
@@ -1,2 +1,24 @@
+// 使用let来声明变量
+let a = "String";
+
+let i = 10;
+
+// let a = 19;
+console.log(a)
+
+// 接收用户名，并打印到控制台
+let username = prompt("请输入用户名")
+console.log("用户名为: " + username);
 
 
+// 交换变量的值
+let x = prompt("请输入第一个变量")
+let y = prompt("请输入第二个变量")
+let t;
+
+t = x;
+x = y;
+y = t;
+
+alert("第一个变量值:" + x)
+alert("第二个变量:" + y)
